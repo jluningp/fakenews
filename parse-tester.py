e499b002df0988d1eeff6a2311cde1a1e574d10d
Jeanne Luning Prak <jluningp@andrew.cmu.edu>
2017-04-23 17:21:03 -0400

    small parser fixes

--- parse-tester.py
+++ parse-tester.py
@@ -6,13 +6,16 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python parse.py [filename]")
         return
+
     p = Parser(sys.argv[1])
     data = p.get_split_data(train_percentage=1.0)
+
     print(data["test"][0])
     print(data["test"][1])
     print(len([x for x in data["train"][1] if x == 0]))
     print(len([x for x in data["train"][1] if x == 1]))
     print(len(data["test"][1]) + len(data["train"][1]))
+    print(data["columns"])
     
 if __name__ == "__main__": main()
  
--- parse.py
+++ parse.py
@@ -2,6 +2,11 @@ import csv
 import numpy as np
 
 class Parser(object):
+
+    # Creates a Parser object
+    # filename: the name of the csv file to be parsed
+    # Instantiates the columns, features, and labels fields by
+    # parsing the csv
     def __init__(self, filename):
         def make_count(a):
             if a == "":
@@ -22,7 +27,9 @@ class Parser(object):
             print("Separating features...")
             vector = [(row[1], row[2:]) for row in feature_reader]
 
+            #The column headings (i.e., "title:Trump")
             self.columns = vector[0][1]
+            
             vector = vector[1:]
             
             #unzips the tuples to make a label set and a feature set
@@ -34,9 +41,11 @@ class Parser(object):
             self.features = features[1:]
             self.labels = labels[1:]
 
-            #The column headings (i.e., "title:Trump")
-            self.columns = features[0]
-
+    # Gets the data split between a train set and a test set
+    # Train percentage set to 85% by default (don't now how we should split it, just guessed lol)
+    # Returns a dict with "train", "test", and "columns" as its entries. Traind and test are lists
+    #         of tuples of (feature vector, label) and columns is the column headings of each
+    #         feature vector (i.e., title:trump)
     def get_split_data(self, train_percentage=0.85):
         #check if train_percentage is valid
         if(train_percentage > 1.0 or train_percentage < 0.0):
