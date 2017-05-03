ff9c6a27601c3d0ca929cdba4a36115347512ec6
Jeanne Luning Prak <jluningp@andrew.cmu.edu>
2017-05-03 13:01:38 -0400

    works with url

--- xml_parser.py
+++ xml_parser.py
@@ -1,5 +1,7 @@
 import numpy as np
 import xml.etree.ElementTree as ET
+import json
+import urllib2
 
 class ParseXML(object):
 
@@ -15,8 +17,9 @@ class ParseXML(object):
 
         self.text_length = 0
         self.title_length = 0
+
         
-    def parse(self):
+    def parsexml(self):
         tree = ET.parse(self.filename)
         root = tree.getroot()
 
@@ -40,6 +43,31 @@ class ParseXML(object):
         for word in self.body_words:
             self.count100["text:" + word] = self.body.count(word)
 
+
+    def getjson(self):
+        response = urllib2.urlopen("http://boilerpipe-web.appspot.com/extract?url=" + self.filename + "&output=json")
+        webContent = response.read()
+        jfile = json.loads(webContent)
+
+        return (jfile["response"]["content"], jfile["response"]["title"])
+            
+    def parse(self):
+        (body, title) = self.getjson()        
+        
+        self.body = body.encode('utf-8').replace(",", "")
+        self.title = title.encode('utf-8').replace(",", "")
+
+        for word in self.title_words:
+            self.count100["title:" + word] = self.title.count(word)
+
+        self.count100["textNUM_TOKENS"] = len(self.body.split())
+        self.count100["titleNUM_TOKENS"] = len(self.title.split())
+            
+        for word in self.body_words:
+            self.count100["text:" + word] = self.body.count(word)
+
+            
+            
     def make_vector(self):
         def get_count(x):
             if x in self.count100:
@@ -48,3 +76,8 @@ class ParseXML(object):
                 return 0
         return np.array([get_count(x) for x in self.columns])
             
+
+    
+p = ParseXML("http://www.theonion.com/article/trump-promises-government-will-continue-fund-all-e-55839", ["text:stuff", "text:Trump", "title:Trump"])
+
+p.getjson()
